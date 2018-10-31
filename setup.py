f00be9abe63278e8ce187ba545265446e577c354
Lawrence Neal <nealla@lwneal.com>
2018-10-31 13:59:02 -0700

    Update to github.com/SCAII/SCAII d9f191c

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='scaii',
-        version='0.1.0',
+        version='0.1.1',
         description='Python bindings for https://github.com/SCAII',
         author='Larry Neal',
         author_email='nealla@lwneal.com',
